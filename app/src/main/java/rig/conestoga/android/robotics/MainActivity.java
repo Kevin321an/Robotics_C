a6491c03ca9426430bdf57b9f86c92e6fbd8cb12
Kevin321an <kevin321an@gmail.com>
2015-07-22 11:00:04 -0700

    First commit

---
+++ app/src/main/java/rig/conestoga/android/robotics/MainActivity.java
@@ -0,0 +1,227 @@
+package rig.conestoga.android.robotics;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        private View rootView;
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            if (sectionNumber==1)
+            {rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                setupListennerHand();}
+            else {
+                rootView = inflater.inflate(R.layout.wrist, container, false);
+            }
+            return rootView;
+        }
+        private Button index,middle,nameless,little,thumb,all_thumb;
+        public void setupListennerHand(){
+
+            index=(Button)rootView.findViewById(R.id.index);
+            middle=(Button)rootView.findViewById(R.id.middle);
+            nameless=(Button)rootView.findViewById(R.id.nameless);
+            little=(Button)rootView.findViewById(R.id.little);
+            thumb=(Button)rootView.findViewById(R.id.thumb);
+            all_thumb=(Button)rootView.findViewById(R.id.all_thumb);
+
+
+
+            index.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    sendCommand(" index");
+                }
+            });
+            middle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    sendCommand(" middle");
+                }
+            });
+            nameless.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    sendCommand(" nameless");
+                }
+            });
+            little.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    sendCommand(" little");
+                }
+            });
+            thumb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    sendCommand(" thumb");
+                }
+            });
+            all_thumb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    sendCommand(" all thumb");
+                }
+            });
+        }
+        public void sendCommand(String part){
+            final String NO_RESULT = getString(R.string.send_command);
+            Toast.makeText(getActivity(), NO_RESULT + part, Toast.LENGTH_SHORT).show();
+        }
+        private int sectionNumber;
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+            sectionNumber=getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        /*viewHolder. playPause.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                ViewHolder viewHolder = new ViewHolder();
+                if (!isPlaying) {
+                    play(viewHolder.playPause);
+                    viewHolder.playPause.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_media_pause));
+
+                } else {
+                    mediaPlayer.pause();
+                    //playPause.setImageResource(getResources().getDrawable(android.R.drawable.presence_busy));
+                    viewHolder.playPause.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_media_play));
+                }
+                isPlaying = !isPlaying;
+            }
+        });*/
+    }
+
+}
